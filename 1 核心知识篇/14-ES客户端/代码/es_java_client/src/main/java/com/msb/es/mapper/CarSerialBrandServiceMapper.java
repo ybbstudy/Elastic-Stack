6871e1bf0977ad7afee3af8411d6f4395d1ed2e9
lei.wu <wlei0618@126.com>
2021-12-23 17:57:28 +0800

    客户端代码更新

--- 1 核心知识篇/14-ES客户端/代码/es_java_client/src/main/java/com/msb/es/mapper/CarSerialBrandServiceMapper.java
+++
@@ -1,7 +0,0 @@
-package com.msb.es.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.msb.es.entity.CarSerialBrand;
-
-public interface CarSerialBrandServiceMapper extends BaseMapper<CarSerialBrand> {
-}
--- 1 核心知识篇/14-ES客户端/代码/es_java_client/src/main/java/com/msb/es/mapper/ProductMapper.java
+++
@@ -1,7 +0,0 @@
-package com.msb.es.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.msb.es.entity.Product;
-
-public interface ProductMapper extends BaseMapper<Product> {
-}
